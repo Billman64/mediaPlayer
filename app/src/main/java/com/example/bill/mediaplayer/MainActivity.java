08d50537370d71c94005f4e674729063902f8223
Bill Lugo <striderbillman@gmail.com>
2023-09-19 17:23:13 -0400

    - Fast forward limit check implemented. - Pause button disabled when media sample completes playing. Fixes pause bug.  - Null check added. - Logs added.

--- app/src/main/java/com/example/bill/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/bill/mediaplayer/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +19,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MediaPlayer_demo";
     public int position=0;
 
     @Override
@@ -42,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "End of song!", Toast.LENGTH_SHORT).show();
                 Button ff = (Button)findViewById(R.id.ff);
                 ff.setEnabled(false);
+                Button pause = (Button) findViewById(R.id.pause);
+                pause.setEnabled(false);
             }
         });
 
@@ -54,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mediaPlayer.start();
+                Log.d(TAG, "Media started. Duration" + mediaPlayer.getDuration());
 //                String title = mediaPlayer.
 //                tv.setText("playing");
             }
@@ -62,11 +67,15 @@ public class MainActivity extends AppCompatActivity {
         // implement pause button functionality
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {    //TODO: fix bug where hitting pause crashes app (might need a status check before pausing/re-pausing)
-                mediaPlayer.pause();
-                position = mediaPlayer.getCurrentPosition();
+            public void onClick(View view) {
+                Log.d(TAG, "Pause button clicked");
+                if(mediaPlayer != null) {
+                    Log.d(TAG, "position: " + mediaPlayer.getCurrentPosition() + " duration: " + mediaPlayer.getDuration());
+                    mediaPlayer.pause();
+                    position = mediaPlayer.getCurrentPosition();
 
-                tv.setText("paused at " + Integer.toString(position));
+                    tv.setText("paused at " + Integer.toString(position));
+                }
 
 
             }
@@ -77,10 +86,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 position = mediaPlayer.getCurrentPosition();
-                mediaPlayer.seekTo(position+10000);
-                tv.setText("FF to position: " + position);
+                int FfAdvance = 10000;
 
-
+                if(position + FfAdvance <= mediaPlayer.getDuration()) {
+                    mediaPlayer.seekTo(position + FfAdvance);
+                    tv.setText("FF to position: " + position);
+                } else {
+                    Log.d(TAG, "Fast forward cancelled, otherwise it would seek past the end");
+                }
 
             }
         });
