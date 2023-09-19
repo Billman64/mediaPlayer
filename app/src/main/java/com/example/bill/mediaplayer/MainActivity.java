5723f08d0dfe6797ffbec825817a7875154969c6
Bill Lugo <striderbillman@gmail.com>
2023-09-19 18:49:37 -0400

    - Pause and FF buttons re-enabled when play is pressed (good for replays). - Resource string for media started message. - UI changes: standard margin dimen set, status output moved to below buttons, views moved to bottom (to make space for future elements)

--- app/src/main/java/com/example/bill/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/bill/mediaplayer/MainActivity.java
@@ -7,7 +7,6 @@ package com.example.bill.mediaplayer;
 //        http://creativecommons.org/licenses/by/4.0/
 
 
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,7 +14,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,9 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button play = (Button)findViewById(R.id.play);
-        Button pause = (Button)findViewById(R.id.pause);
-        Button ff = (Button)findViewById(R.id.ff);
+        Button play = findViewById(R.id.play);
+        Button pause = findViewById(R.id.pause);
+        Button ff = findViewById(R.id.ff);
+        final TextView tv = findViewById(R.id.out);
 
 
 
@@ -39,19 +38,16 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
-                mediaPlayer.release();
-                mediaPlayer = null;
-                Toast.makeText(MainActivity.this, "End of song!", Toast.LENGTH_SHORT).show();
-                Button ff = (Button)findViewById(R.id.ff);
+//                mediaPlayer.reset();
+                tv.setText(R.string.media_completed);
+//                Toast.makeText(MainActivity.this, "End of song!", Toast.LENGTH_SHORT).show();
+                Button ff = findViewById(R.id.ff);
                 ff.setEnabled(false);
-                Button pause = (Button) findViewById(R.id.pause);
+                Button pause = findViewById(R.id.pause);
                 pause.setEnabled(false);
             }
         });
 
-
-
-        final TextView tv = (TextView) findViewById(R.id.out);
 
         // implement play button functionality
         play.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +56,11 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.start();
                 Log.d(TAG, "Media started. Duration" + mediaPlayer.getDuration());
 //                String title = mediaPlayer.
-//                tv.setText("playing");
+                tv.setText("playing");
+
+                // ensure buttons enabled (for replays)
+                pause.setEnabled(true);
+                ff.setEnabled(true);
             }
         });
 
